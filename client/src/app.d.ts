d41f7d35bc9d63b5f97513ea32cb3fe9134abe15
guicoelhodev <gs.coelho_dev@outlook.com>
2026-04-21 23:25:04 -0300

    build: scaffold SvelteKit project with config and static assets

---
+++ client/src/app.d.ts
@@ -0,0 +1,18 @@
+// See https://svelte.dev/docs/kit/types#app.d.ts
+// for information about these interfaces
+declare global {
+	namespace App {
+		interface Env {
+			GITHUB_TOKEN: string;
+			GITHUB_OWNER: string;
+			GITHUB_REPO: string;
+		}
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
+}
+
+export {};
